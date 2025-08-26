7b6f1c2302a4bfeb318a6eae63cdb25dcad8d65f
Anton Istomin <istomanton@gmail.com>
2025-08-26 22:09:15 +0200

    chore: tweak send ff loop prog

--- examples/CARGON_PROG_TN_SEND_FF_LOOP.s
+++ examples/CARGON_PROG_TN_SEND_FF_LOOP.s
@@ -1,9 +1,9 @@
 // This program sends 0xFF to RECIPIENT_ADDR and waits for an answer. If the answer is 0xFF it sends it again and so on until iteration 255.
-// If the answer is not 0xFF then it halts. Keep in mind that 0 answer is the same as no packet due to adapter limitations, so the program will retry on 0 answer.
+// If the answer is not 0xFF then it halts.
 // If it halts, r1 will contain the last number received over the network.
 // Set RECIPIENT_ADDR to this CPU's address to send packets to itself.
 
-@define RECIPIENT_ADDR 42
+@define RECIPIENT_ADDR 5 // todo set back to 42 after node v1.1.1 is fixed
 @define NUMBER_TO_SEND 0xFF
 @define MAX_ITERATIONS 255
 @define START_ITERATION 0
@@ -46,17 +46,16 @@ pst @TN_COMMAND_PORT // Turn the TN interface on
 lim r0 @NUMBER_TO_SEND
 pst @TN_WRITE_DATA_PORT
 lim r0 @RECIPIENT_ADDR
-pst @TN_RECEPIENT_ADDR_PORT
+pst @TN_RECEPIENT_ADDR_PORT // Send the number to the recipient
 
 .receive_loop
 pld @TN_STATUS_PORT
 lim r1 @TN_MORE_PACKETS_BIT
 and r1
-brc eq .receive_loop // If bit 2 is not set, wait for a packet to be received
+brc eq .receive_loop // If more packets bit is not set, wait for a packet to be received
 
 lim r0 @TN_COMMAND_NEXT_PACKET
 pst @TN_COMMAND_PORT // Next packet command
-pst @TN_NEXT_DATA_PORT // Write anything to TN_NEXT_DATA_PORT to trigger next data byte command
 pld @TN_READ_DATA_PORT // Read data from input port into accumulator
 pst @OUTPUT_PORT // Output the received number
 
